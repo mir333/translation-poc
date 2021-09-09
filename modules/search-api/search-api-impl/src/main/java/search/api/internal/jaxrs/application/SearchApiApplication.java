de9ea69200aa7096c04002f2c968578a07d41134
Miroslav Ligas <miroslav@ligas.im>
2021-09-09 15:29:38 +0100

    search

---
+++ modules/search-api/search-api-impl/src/main/java/search/api/internal/jaxrs/application/SearchApiApplication.java
@@ -0,0 +1,23 @@
+package search.api.internal.jaxrs.application;
+
+import javax.annotation.Generated;
+
+import javax.ws.rs.core.Application;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author ligasm
+ * @generated
+ */
+@Component(
+	property = {
+		"liferay.jackson=false", "osgi.jaxrs.application.base=/search-api",
+		"osgi.jaxrs.extension.select=(osgi.jaxrs.name=Liferay.Vulcan)",
+		"osgi.jaxrs.name=SearchApi"
+	},
+	service = Application.class
+)
+@Generated("")
+public class SearchApiApplication extends Application {
+}
