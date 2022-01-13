3c7b1c930f1e1e9bdf32d501e5135455e1100cc8
Miroslav Ligas <miroslav@ligas.im>
2022-01-13 19:56:59 +0000

    liferay ui search alignment

--- modules/search-api/search-api-impl/src/main/java/custom/search/api/internal/resource/v1_0/SearchResourceImpl.java
+++ modules/search-api/search-api-impl/src/main/java/custom/search/api/internal/resource/v1_0/SearchResourceImpl.java
@@ -6,6 +6,10 @@ import com.liferay.knowledge.base.model.KBArticle;
 import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
 import com.liferay.portal.search.engine.adapter.search.SearchSearchRequest;
 import com.liferay.portal.search.engine.adapter.search.SearchSearchResponse;
+import com.liferay.portal.search.highlight.FieldConfig;
+import com.liferay.portal.search.highlight.Highlight;
+import com.liferay.portal.search.highlight.HighlightBuilder;
+import com.liferay.portal.search.highlight.HighlightBuilderFactory;
 import com.liferay.portal.search.hits.SearchHits;
 import com.liferay.portal.search.query.*;
 import com.liferay.portal.search.query.function.score.ScoreFunctions;
@@ -19,6 +23,8 @@ import custom.search.api.resource.v1_0.SearchResource;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,6 +37,9 @@ import java.util.stream.Collectors;
         scope = ServiceScope.PROTOTYPE, service = SearchResource.class
 )
 public class SearchResourceImpl extends BaseSearchResourceImpl {
+
+    private static Logger LOG = LoggerFactory.getLogger(SearchResourceImpl.class);
+
     @Reference
     private SearchEngineAdapter searchEngineAdapter;
     @Reference
@@ -47,22 +56,33 @@ public class SearchResourceImpl extends BaseSearchResourceImpl {
 
         final BooleanQuery bq = queries.booleanQuery();
 
-//      query.addMustQueryClauses(queries.match("title_en_US", variation));
-//		MatchAllQuery query = queries.matchAll();
-//		WildcardQuery query = queries.wildcard("content_en_US", variation);
-
-        SimpleStringQuery query = queries.simpleString(variation);
-        query.addFields("*");
+        StringQuery query = queries.string(variation);
+        query.setEscape(false);
         bq.addMustQueryClauses(query);
 
+        final BooleanQuery fileEntryFilter = queries.booleanQuery();
+        fileEntryFilter.addMustQueryClauses(queries.term("entryClassName","com.liferay.document.library.kernel.model.DLFileEntry"));
+        fileEntryFilter.addMustQueryClauses(queries.term("hidden","false"));
+        fileEntryFilter.addMustQueryClauses(queries.term("status","0"));
 
-        TermsQuery terms= queries.terms("entryClassName");
-        terms.addValues(JournalArticle.class.getName());
-        terms.addValues(DLFileEntry.class.getName());
-        terms.addValues(KBArticle.class.getName());
-        bq.addMustQueryClauses(terms);
+        final BooleanQuery articleFilter = queries.booleanQuery();
+        articleFilter.addMustQueryClauses(queries.term("entryClassName","com.liferay.journal.model.JournalArticle"));
+        articleFilter.addMustQueryClauses(queries.term("head","true"));
+        final TermsQuery tq = queries.terms("status");
+        tq.addValue("0");
+        articleFilter.addMustQueryClauses(tq);
+        articleFilter.addShouldQueryClauses(queries.dateRangeTerm("expirationDate",false,false,"20220113161902",null));
 
-        bq.addShouldQueryClauses(queries.match("latest", "true"));
+        final BooleanQuery kbArticleFilter = queries.booleanQuery();
+        kbArticleFilter.addMustQueryClauses(queries.term("entryClassName","com.liferay.knowledge.base.model.KBArticle"));
+
+        final BooleanQuery filterBg = queries.booleanQuery();
+        filterBg.addShouldQueryClauses(fileEntryFilter);
+        filterBg.addShouldQueryClauses(articleFilter);
+        filterBg.addShouldQueryClauses(kbArticleFilter);
+
+        bq.addFilterQueryClauses(queries.booleanQuery().addMustQueryClauses(filterBg));
+
 
 //		SortBuilder sortBuilder = factory.getSortBuilder();
 //		final Sort ratings = sortBuilder.field("ratings").sortOrder(SortOrder.ASC).build();
@@ -73,23 +93,22 @@ public class SearchResourceImpl extends BaseSearchResourceImpl {
 //        final ScriptScoreFunction scoreFunction = scoreFunctions.script(script);
 //        functionScoreQuery.addFilterQueryScoreFunctionHolder(null, scoreFunction);
 
-        final Script script2 = scripts.script( "try{ if(doc.containsKey('ratings_Number_sortable')){1 + Math.log(1 + doc['ratings_Number_sortable'].value)} } catch(Exception e) {}");
+        final Script script2 = scripts.script( "try{ if(doc.containsKey('ratings_Number_sortable')){_score + (_score /100) * 1 + Math.log(1 + doc['ratings_Number_sortable'].value)} } catch(Exception e) {}");
         final ScriptScoreFunction scoreFunction2 = scoreFunctions.script(script2);
         functionScoreQuery.addFilterQueryScoreFunctionHolder(null, scoreFunction2);
 
         functionScoreQuery.setScoreMode(FunctionScoreQuery.ScoreMode.MAX);
-
-
 
         SearchSearchRequest searchSearchRequest =
                 new SearchSearchRequest();
 
         searchSearchRequest.setIndexNames("_all");
         searchSearchRequest.setQuery(functionScoreQuery);
-        searchSearchRequest.setSize(200);
+        searchSearchRequest.setSize(20);
         searchSearchRequest.setScoreEnabled(true);
-        searchSearchRequest.setFetchSourceIncludes(new String[]{"entryClassName", "title_en_US", "entryClassPK", "priority_sortable", "ratings","ratings_Number_sortable"});
 
+        searchSearchRequest.setTrackTotalHits(true);
+        searchSearchRequest.setExplain(true);
 
         SearchSearchResponse searchSearchResponse =
                 searchEngineAdapter.execute(searchSearchRequest);
@@ -101,6 +120,9 @@ public class SearchResourceImpl extends BaseSearchResourceImpl {
             search.setScore(String.valueOf(searchHit.getScore()));
             return search;
         }).collect(Collectors.toList());
+
+        LOG.error("Request: {}",searchSearchResponse.getSearchRequestString());
+        LOG.error("Response: {}",searchSearchResponse.getSearchResponseString());
 
         return Page.of(collect);
 
